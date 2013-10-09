18240791036c6a44e156df2cfe90964f0da1a0fa
Jack Preston <jackprestonuk@gmail.com>
2013-10-09 15:16:51 +0100

    setup.py, nearly there with PyPi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+	name = 'wallpyper',
+	version = '0.0.1',
+	author = 'https://github.com/unwitting',
+	author_email = 'jackprestonuk@gmail.com',
+	packages = ['wallpyper'],
+	scripts = ['bin/randomiser.py'],
+	url = 'https://github.com/unwitting/wallpyper',
+	license = 'LICENSE.txt',
+	description = 'Tiny Python module handling Linux wallpaper changes',
+	long_description = open('README.txt').read(),
+	install_requires = []
+)
